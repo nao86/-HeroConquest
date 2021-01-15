85f27a64d97b4713517417fe997dc611ea917d9b
nao86 <5201051@st.hsc.ac.jp>
2021-01-15 10:03:43 +0900

    12/08

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def Home():
+    return "Hello!!"
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+#スキルチェックページ
+@app.route('/check')
+def check():
+    return render_template('check.html')
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run()
